186b3839204b666745e9e686b97d0190146e0533
cis101 <cis101@secs.iub.edu.bd>
2019-10-09 09:17:34 +0600

    Task 2b

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ console.log(headingText.textContent)
 function addText(event){
     var p = document.createElement('p')
     p.innerHTML = "Hello World"
+    p.style.color = "red"
     var div = document.getElementById('task2a')
     div.appendChild(p)
     console.log(event.target)
 }
+
+function bgChange(event){
+    //var body = document.getElementsByTagName('body')
+    document.body.style.background = event.target.innerText.toLowerCase();
+}
+// function bgGreen(event){
+//     //var body = document.getElementsByTagName('body')
+//     document.body.style.background = "Green";
+// }
 
 
 /* Task 4 using jQuery */
