75735a1fac69b5bf83a23e5596ea110a0b05ab7e
cis101 <cis101@secs.iub.edu.bd>
2019-10-09 09:02:47 +0600

    Task 2a

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ console.log(headingText.textContent)
 
 
 /* Task 2 using plain JavaScript */
-
+function addText(event){
+    var p = document.createElement('p')
+    p.innerHTML = "Hello World"
+    var div = document.getElementById('task2a')
+    div.appendChild(p)
+    console.log(event.target)
+}
 
 
 /* Task 4 using jQuery */
